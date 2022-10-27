3c8430ef00ab5d05f944529440713fd3f991dd07
Debby95 <debbyjacksonstar@hotmail.it>
2022-10-27 10:25:10 +0200

    Stampo lunghezza array

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -4,14 +4,11 @@ using System;
 using System.IO;
 
 string[] cibiPreferiti = { "Pasta", "Pizza", "Broccoli", "Cotoletta" };
-
-
-
-
-
+Console.WriteLine(cibiPreferiti.Length);
 
 
 for (int i = 0; i < cibiPreferiti.Length; i++)
 {
     Console.WriteLine(cibiPreferiti[i]);
 }
+
