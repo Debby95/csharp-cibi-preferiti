557c3bcd2052ef5e9716ede7c7f8397455dee00a
Debby95 <debbyjacksonstar@hotmail.it>
2022-10-27 10:33:38 +0200

    Stampo cibo top e ultimo cibo preferito

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -4,11 +4,13 @@ using System;
 using System.IO;
 
 string[] cibiPreferiti = { "Pasta", "Pizza", "Broccoli", "Cotoletta" };
-Console.WriteLine(cibiPreferiti.Length);
-
 
 for (int i = 0; i < cibiPreferiti.Length; i++)
 {
-    Console.WriteLine(cibiPreferiti[i]);
+    Console.WriteLine("Cibo: " + cibiPreferiti[i]);
 }
+
+Console.WriteLine("I cibi preferiti sono: " + cibiPreferiti.Length);
+Console.WriteLine("Cibo Top: " + cibiPreferiti[0]);
+Console.WriteLine("Ultimo cibo preferito: " + cibiPreferiti[3]);
 
