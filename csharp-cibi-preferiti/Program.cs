66b0cd8d3db7055261e32387f7cebedbe1c6558f
Debby95 <debbyjacksonstar@hotmail.it>
2022-10-27 11:01:15 +0200

    Aggiungo commenti

--- csharp-cibi-preferiti/Program.cs
+++ csharp-cibi-preferiti/Program.cs
@@ -5,15 +5,20 @@ using System.IO;
 
 string[] cibiPreferiti = { "Pasta", "Pizza", "Broccoli", "Cotoletta", "Pane" };
 
+//stampo la lunghezza della lista
+
 Console.WriteLine("I cibi preferiti sono: " + cibiPreferiti.Length);
 
 for (int i = 0; i < cibiPreferiti.Length; i++)
 {
+    //stampo la lista dei cibi preferiti
     Console.WriteLine("Cibo: " + cibiPreferiti[i]);
 }
 
-
+//stampo il cibo top
 Console.WriteLine("Cibo Top: " + cibiPreferiti[0]);
+//stampo l'ultimo cibo preferito
 Console.WriteLine("Ultimo cibo preferito: " + cibiPreferiti[3]);
+//stampo il cibo preferito presente al centro
 Console.WriteLine("Cibo presente al centro: " + cibiPreferiti[2]);
 
